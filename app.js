bb2991162e9b7169682ff43b59e4048a2c1eb3b0
jponichter <ponichterjot@gmail.com>
2019-12-08 02:14:00 +0100

    Updates

--- app.js
+++ app.js
@@ -1,64 +1,75 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const top = document.querySelector("nav");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  const burgerColor = document.querySelectorAll("line");
+"use strict";
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+var navSlide = function navSlide() {
+  var burger = document.querySelector(".burger");
+  var nav = document.querySelector(".nav-links");
+  var top = document.querySelector("nav");
+  var navLinks = document.querySelectorAll(".nav-links li");
+  var burgerColor = document.querySelectorAll("line");
 
   function animateLinks() {
-    const screen = window.matchMedia("(max-width: 768px)");
-    navLinks.forEach((link, index) => {
+    var screen = window.matchMedia("(max-width: 768px)");
+    navLinks.forEach(function(link, index) {
       if (link.style.animation && screen.matches) {
         link.style.animation = "";
       } else if (screen.matches) {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          0.7}s`;
+        link.style.animation = "navLinkFade 0.5s ease forwards ".concat(
+          index / 7 + 0.7,
+          "s"
+        );
       }
     });
-  }
+  } //Toggle Nav
 
-  //Toggle Nav
-  burger.addEventListener("click", () => {
+  burger.addEventListener("click", function() {
     nav.classList.toggle("nav-active");
+
     if (window.matchMedia("(max-width: 768px)").matches) {
       top.classList.toggle("nav-black");
     }
-    burgerColor.forEach(index => {
+
+    burgerColor.forEach(function(index) {
       index.classList.toggle("burger-white");
     });
     animateLinks();
-  });
+  }); //Toggle while clicking on links
 
-  //Toggle while clicking on links
   function linkToggle() {
-    navLinks.forEach((link, index) => {
-      link.addEventListener("click", () => {
+    navLinks.forEach(function(link, index) {
+      link.addEventListener("click", function() {
         nav.classList.toggle("nav-active");
+
         if (window.matchMedia("(max-width: 768px)").matches) {
           top.classList.toggle("nav-black");
         }
-        burgerColor.forEach(index => {
+
+        burgerColor.forEach(function(index) {
           index.classList.remove("burger-white");
         });
         animateLinks();
       });
     });
   }
+
   linkToggle();
 };
 
 navSlide();
 
 function smoothScroll(target, duration) {
-  let targetSection = document.querySelector(target);
-  let targetPosition = targetSection.getBoundingClientRect().top;
-  let startingPosition = window.pageYOffset;
-  let startTime = null;
+  var targetSection = document.querySelector(target);
+  var targetPosition = targetSection.getBoundingClientRect().top;
+  var startingPosition = window.pageYOffset;
+  var startTime = null;
 
   function animation(currentTime) {
     if (startTime === null) startTime = currentTime;
-    let timeElapsed = currentTime - startTime;
-    let run = ease(timeElapsed, startingPosition, targetPosition, duration);
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startingPosition, targetPosition, duration);
     window.scrollTo(0, run);
     if (timeElapsed < duration) requestAnimationFrame(animation);
   }
@@ -76,82 +87,78 @@ function smoothScroll(target, duration) {
 document.querySelector(".link-about").addEventListener("click", function() {
   smoothScroll("#about-page", 1000);
 });
-
 document.querySelector(".link-portfolio").addEventListener("click", function() {
   smoothScroll("#portfolio-page", 1000);
 });
-
 document.querySelector(".link-contact").addEventListener("click", function() {
   smoothScroll("#contact-page", 1000);
 });
 
 function navScroller() {
-  const nav = document.querySelector("nav");
-  const burger = document.querySelectorAll("line");
-  const aboutPage = document.querySelector("#about-page");
-  const portfolioPage = document.querySelector("#portfolio-page");
-  const frontPage = document.querySelector(".front-page");
-  const footer = document.querySelector("footer");
-
-  const ScrollOneOptions = {
+  var nav = document.querySelector("nav");
+  var burger = document.querySelectorAll("line");
+  var aboutPage = document.querySelector("#about-page");
+  var portfolioPage = document.querySelector("#portfolio-page");
+  var frontPage = document.querySelector(".front-page");
+  var footer = document.querySelector("footer");
+  var ScrollOneOptions = {
     rootMargin: "-100px 0px 0px 0px"
   };
-  const ScrollTwoOptions = {
+  var ScrollTwoOptions = {
     threshold: 0,
     rootMargin: "-30px 0px 0px 0px"
   };
-  const ScrollThreeOptions = {
+  var ScrollThreeOptions = {
     threshold: 0,
     rootMargin: "-30px 0px 0px 0px"
   };
-
-  const ScrollOneObserver = new IntersectionObserver(
-    (entries, ScrollOneObserver) => {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          nav.classList.add("nav-scrolled");
-        } else {
-          nav.classList.remove("nav-scrolled");
-        }
-      });
-    },
-    ScrollOneOptions
-  );
-
-  const ScrollTwoObserver = new IntersectionObserver(
-    (entries, ScrollTwoObserver) => {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          burger.forEach((item, index) => {
-            item.classList.add("burger-scrolled");
-          });
-        } else {
-          burger.forEach((item, index) => {
-            item.classList.remove("burger-scrolled");
-          });
-        }
-      });
-    },
-    ScrollTwoOptions
-  );
-
-  const ScrollThreeObserver = new IntersectionObserver(
-    (entries, ScrollThreeObserver) => {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          burger.forEach((item, index) => {
-            item.classList.remove("burger-scrolled");
-          });
-        } else {
-          burger.forEach((item, index) => {
-            item.classList.add("burger-scrolled");
-          });
-        }
-      });
-    },
-    ScrollThreeOptions
-  );
-
+  var ScrollOneObserver = new IntersectionObserver(function(
+    entries,
+    ScrollOneObserver
+  ) {
+    entries.forEach(function(entry) {
+      if (!entry.isIntersecting) {
+        nav.classList.add("nav-scrolled");
+      } else {
+        nav.classList.remove("nav-scrolled");
+      }
+    });
+  },
+  ScrollOneOptions);
+  var ScrollTwoObserver = new IntersectionObserver(function(
+    entries,
+    ScrollTwoObserver
+  ) {
+    entries.forEach(function(entry) {
+      if (!entry.isIntersecting) {
+        burger.forEach(function(item, index) {
+          item.classList.add("burger-scrolled");
+        });
+      } else {
+        burger.forEach(function(item, index) {
+          item.classList.remove("burger-scrolled");
+        });
+      }
+    });
+  },
+  ScrollTwoOptions);
+  var ScrollThreeObserver = new IntersectionObserver(function(
+    entries,
+    ScrollThreeObserver
+  ) {
+    entries.forEach(function(entry) {
+      if (!entry.isIntersecting) {
+        burger.forEach(function(item, index) {
+          item.classList.remove("burger-scrolled");
+        });
+      } else {
+        burger.forEach(function(item, index) {
+          item.classList.add("burger-scrolled");
+        });
+      }
+    });
+  },
+  ScrollThreeOptions);
   ScrollOneObserver.observe(frontPage);
   ScrollTwoObserver.observe(aboutPage);
   ScrollThreeObserver.observe(portfolioPage);
